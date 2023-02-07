713fc9aab17b1b74b95914d551734f22bd06aa3a
Marika <marika.bergman@gmail.com>
2023-02-07 10:59:09 +0000

    add footer component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,6 @@
-import Sidebar from "../Sidebar"
-import catFood from "../../assets/images/dry-cat-food.jpg"
+import Sidebar from "./Sidebar"
+import Footer from "./Footer"
+import catFood from "../assets/images/dry-cat-food.jpg"
 
 const Layout = () => {
 
@@ -21,6 +22,11 @@ const Layout = () => {
   function w3_open() {
     document.getElementById("mySidebar").style.display = "block";
     document.getElementById("myOverlay").style.display = "block";
+  }
+
+  function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
   }
 
 
@@ -45,9 +51,9 @@ const Layout = () => {
         }><i className="fa fa-bars"></i></a>
       </header>
 
-      {/*Overlay effect when opening sidebar on small screens 
-      <div className="w3-overlay w3-hide-large" onClick={w3_close()} style="cursor:pointer" title="close side menu" id="myOverlay"></div>
-        */}
+      {/*Overlay effect when opening sidebar on small screens */}
+      <div className="w3-overlay w3-hide-large" onClick={w3_close} style={{ cursor: "pointer" }} title="close side menu" id="myOverlay"></div>
+
 
       {/* !PAGE CONTENT! */}
       <div className="w3-main" style={{ marginLeft: "250px" }}>
@@ -148,49 +154,7 @@ const Layout = () => {
 
         {/* Footer */}
         <footer className="w3-padding-64 w3-light-grey w3-small w3-center" id="footer">
-          <div className="w3-row-padding">
-            <div className="w3-col s4">
-              <h4>Contact</h4>
-              <p>Questions? Go ahead.</p>
-              <form action="/action_page.php" target="_blank">
-                <p><input className="w3-input w3-border" type="text" placeholder="Name" name="Name" required /></p>
-                <p><input className="w3-input w3-border" type="text" placeholder="Email" name="Email" required /></p>
-                <p><input className="w3-input w3-border" type="text" placeholder="Subject" name="Subject" required /></p>
-                <p><input className="w3-input w3-border" type="text" placeholder="Message" name="Message" required /></p>
-                <button type="submit" className="w3-button w3-block w3-black">Send</button>
-              </form>
-            </div>
-
-            <div className="w3-col s4">
-              <h4>About</h4>
-              <p><a href="#">About us</a></p>
-              <p><a href="#">We're hiring</a></p>
-              <p><a href="#">Support</a></p>
-              <p><a href="#">Find store</a></p>
-              <p><a href="#">Shipment</a></p>
-              <p><a href="#">Payment</a></p>
-              <p><a href="#">Gift card</a></p>
-              <p><a href="#">Return</a></p>
-              <p><a href="#">Help</a></p>
-            </div>
-
-            <div className="w3-col s4 w3-justify">
-              <h4>Store</h4>
-              <p><i className="fa fa-fw fa-map-marker"></i> Company Name</p>
-              <p><i className="fa fa-fw fa-phone"></i> 0044123123</p>
-              <p><i className="fa fa-fw fa-envelope"></i> ex@mail.com</p>
-              <h4>We accept</h4>
-              <p><i className="fa fa-fw fa-cc-amex"></i> Amex</p>
-              <p><i className="fa fa-fw fa-credit-card"></i> Credit Card</p>
-              <br />
-              <i className="fa fa-facebook-official w3-hover-opacity w3-large"></i>
-              <i className="fa fa-instagram w3-hover-opacity w3-large"></i>
-              <i className="fa fa-snapchat w3-hover-opacity w3-large"></i>
-              <i className="fa fa-pinterest-p w3-hover-opacity w3-large"></i>
-              <i className="fa fa-twitter w3-hover-opacity w3-large"></i>
-              <i className="fa fa-linkedin w3-hover-opacity w3-large"></i>
-            </div>
-          </div>
+          <Footer />
         </footer>
 
         <div className="w3-black w3-center w3-padding-24">Powered by <a href="https:{/*www.w3schools.com/w3css/default.asp" title="W3.CSS" target="_blank" className="w3-hover-opacity">w3.css</a></div>
